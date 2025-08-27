5561d72d8b1bbde59775152bfb6f914c046a627e
imran <hasan.imran562@gmail.com>
2025-08-27 23:40:28 +0530

    added the changes in constant and gitignore

--- src/constant.js
+++ src/constant.js
@@ -1,3 +1,4 @@
 // API keys and context keys
+// only 100 api calls possible with this api
 export const API_KEY = 'AIzaSyBpTklh_7uqi14rwCKnNcAPxb9CDcmbaZ4';
 export const SEARCH_KEY = '2057eb3f30af04b1e';
